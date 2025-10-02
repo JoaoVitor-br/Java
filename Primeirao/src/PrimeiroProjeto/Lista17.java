056c2db9f301f418bdbebb34c514b10ae6d0aeff
JoaoVitor-br <amorim.jao3@gmail.com>
2025-10-02 10:39:49 -0300

    .

---
+++ Primeirao/src/PrimeiroProjeto/Lista17.java
@@ -0,0 +1,21 @@
+package PrimeiroProjeto;
+
+import java.util.Scanner;
+
+public class Lista17 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+//		1 – Criando uma matriz simples
+		int[][] inteiro = new int[3][2];
+		for(int e = 0; e<3; e++) {
+			for(int r = 0; r<2; r++) {
+				System.out.println("Digite um numero: ");
+				
+			}
+		}
+		
+	}
+
+}
