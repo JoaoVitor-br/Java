84d0ff95855fde3b67dca4436ad8ced8b1ded294
JoaoVitor-br <amorim.jao3@gmail.com>
2025-10-17 16:06:50 -0300

    .

--- Primeirao/src/PrimeiroProjeto/Lista20.java
+++ Primeirao/src/PrimeiroProjeto/Lista20.java
@@ -1,23 +1,106 @@
 package PrimeiroProjeto;
 
+import java.util.Scanner;
+
 public class Lista20 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
-		for(int e =0;e <5;e++) {
-			Bia();
-		}for(int e = 0; e < 6; e++){
-			retangolo();
-		}
-
-
+		for(int e =0;e <5;e++) 
+		Bia();
+		retangolo();
+		vintecinco();
+		oito();
+		mes();
+		System.out.println("digite um numero: ");
+		int cinco = scanner.nextInt();
+		divisivel(cinco);
+		System.out.println("DIGITE O SEU NOME:");
+		String voutar = scanner.next();
+		thiau(voutar);
+		System.out.println("Digite o seu nome:");
+		String sonic = scanner.next();
+		System.out.println("Digite o seu sobrenome");
+		String mauro = scanner.next();
+		emon(sonic,mauro);
+		System.out.println("Escreva um numero: ");
+		int varificar = scanner.nextInt();
+		virificacao(varificar);
+		System.out.println("Quantos kilometro voce esta: ");
+		int velocimetro = scanner.nextInt();
+		veloicidade(velocimetro);
+		System.out.println("Qual e o dia da semana: ");
+		String homencalendario = scanner.next();
+		semanal(homencalendario);
+		System.out.println("Qual a situação do estoque: ");		
+		int ssssss = scanner.nextInt();
+		estoque(ssssss);
+		
 	}
 	public static void Bia() {
+		for(int e =0;e <5;e++) {
 		System.out.println("Bom dia!");
+		}
 	}
 	public static void retangolo() {
-		System.out.print("************\n");
+		for(int e = 0; e < 6; e++) {
+			System.out.print("************\n");
+			}
 	}
-
-
+	public static void vintecinco() {
+		int num =5;
+		for(int e=0; e<=5; e++) {
+			System.out.println((num*e));
+		}
+	}
+	public static void oito() {
+		for (int e =1;e <=8;e++) {
+			System.out.println(e);
+			System.out.println("pronto");
+		}
+	}
+	public static void mes() {
+	System.out.println("Janeiro");	
+	System.out.println("Fevereiro");
+	System.out.println("Março");
+	System.out.println("Abril");
+	System.out.println("Maio");
+	System.out.println("Junho");
+	}public static void divisivel(int num){
+		if (num%5 ==0) {
+			System.out.println((num/5));
+		}else {
+			System.out.println("Não e divisivel por cinco");
+		}
+	}public static void thiau(String nome) {
+		System.out.println("Até logo "+ nome);
+	}public static void emon(String no,String me) {
+		System.out.println(no+me);
+	}public static void virificacao(int cem) {
+		if(cem==100) {
+			System.out.println("E cem!!");
+		}else {
+			System.out.println("Não e cem.");
+		}
+	}public static void veloicidade(int veloz) {
+		if (veloz <40) {
+			System.out.println("Lenta"); 
+		}else if(veloz >40 && veloz <80) {
+			System.out.println("Normar");
+		}else {
+			System.out.println("Rapida");
+		}
+	}public static void semanal(String semana) {
+		System.out.println("Tenha um ótima"+semana);
+	}public static void estoque(int estocado) {
+		if(estocado >=10) {
+			System.out.println("Estoque suficiente.");
+		}else if(estocado < 10 && estocado >5) {
+			System.out.println("Estoque baixo");
+		}else {
+			System.out.println("estoque critico.");
+		}
+	}
+	
 }
